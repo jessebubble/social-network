b6815f9999102a1ca4bb6e1ec366f51f16f55b3a
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-03-10 14:00:20 -0600

    user model updated

--- models/User.js
+++ models/User.js
@@ -1,28 +1,36 @@
 const { Schema, model } = require('mongoose');
 
+// create SCHEMA 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // MATCH - RegExp, creates a validator that checks if the value matches the given regular expression
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }], // Array of _id values referencing the Thought model
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }] // Array of _id values referencing the User model (self-reference)
+    },
+    {
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
 
-/* 
-username ()
-String
-Unique
-Required
-Trimmed
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
-email ()
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
-
-thoughts ()
-Array of _id values referencing the Thought model
-
-friends ()
-Array of _id values referencing the User model (self-reference)
-
-
-Schema Settings
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-*/
+// Create USER model using UserSchema
+const User = model('User', UserSchema);
 
 module.exports = User;
